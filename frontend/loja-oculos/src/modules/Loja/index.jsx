2da9dba40e93e857c0242bf518c604aca2b7c81d
pabloolvr <48020553+pabloolvr@users.noreply.github.com>
2021-05-22 21:12:14 -0300

    a

--- frontend/loja-oculos/src/modules/Loja/index.jsx
+++ frontend/loja-oculos/src/modules/Loja/index.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { Card } from '../common/Card'
 
 export const Loja = () => {
-  return <div>Loja</div>;
+  return <div className="loja">Loja
+    <div className="products-wrapper">
+      <div className="shell">
+        <div className="wrapper-left">
+          aaa
+        </div>
+          <div className="wrapper-right">
+            <ul>
+              <li>
+                <div><Card></Card></div>
+              </li>
+              <li>
+                <div><Card></Card></div>
+              </li>
+              <li>
+                <div><Card></Card></div>
+              </li>
+              <li>
+                <div><Card></Card></div>
+              </li>
+              <li>
+                <div><Card></Card></div>
+              </li>
+              <li>
+                <div><Card></Card></div>
+              </li>
+            </ul>
+        </div>
+      </div>
+
+
+    </div>
+  </div>;
 };
