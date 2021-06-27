3fb9cd8f91c6a0086c0fa5a4363ff55c83f7b7f6
Victor Hugo <victorsilva698@usp.br>
2021-06-27 20:41:19 -0300

    updates cards

--- frontend/loja-oculos/src/modules/Loja/index.jsx
+++ frontend/loja-oculos/src/modules/Loja/index.jsx
@@ -1,37 +1,24 @@
 import React from "react";
 import "./style.css";
 import { Card } from "../common/Card";
-
+import { SideOption } from '../common/SideOptions'
+import { dados } from '../../mocks/loja'
 export const Loja = () => {
+
+
+
+
   return (
     <div className="loja">
-      Loja
       <div className="products-wrapper">
         <div className="shell">
-          <div className="wrapper-left">
-            <h2>ÓCULOS DE SOL</h2>
-            <div className="filter">
-              <div className="filter-nametext">
-                <h2>FILTRAR</h2>
-              </div>
-              <div className="navigator">
-                <h4>Por Gênero</h4>
-                <h4>Material da Lente</h4>
-                <h4>Por estilo</h4>
-                <h4>Cor da Armação</h4>
-                <h4>Por coleção</h4>
-                <h4>Faixa de Preço</h4>
-              </div>
-            </div>
-          </div>
+          <SideOption></SideOption>
           <div className="wrapper-right">
-            <ul>
-              <li>
-                <div className="shelf">
-                  <Card name="Oculos Caro" price={29.90}></Card>
-                </div>
-              </li>
-            </ul>
+            {dados.map((item)=>{
+              return(
+                <Card key={item.id.toString()}props={item}></Card>
+              )
+            })}
           </div>
         </div>
       </div>
